933d2be993ebe1caa222440904a1d47f6ae49e06
normchou <normchou@users.noreply.github.com>
2017-06-20 10:26:34 -0700

    set up basic react app

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+
+const Root = () => (
+	<Router>
+		<Route path='/' component={Home} />
+	</Router>
+);
+
+export default Root;
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+	const Root = require('./components/Root.js').default;
+
+	ReactDOM.render(<Root />, rootEl)
+};
+
+if (module.hot) {
+	const renderApp = render;
+	const renderError = err => {
+		const RedBox = require('redbox-react');
+
+		ReactDOM.render(<RedBox error={err} />, rootEl);
+	};
+
+	render = () => {
+		try {
+			renderApp();
+		} catch (err) {
+			renderError(err);;
+		}
+	}
+
+	module.hot.accept('./components/Root', render);
+}
+
+render();
