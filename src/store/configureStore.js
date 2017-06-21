b4a44e0492e981cc94f3b9c151f058f998fefdaf
normchou <normchou@users.noreply.github.com>
2017-06-21 09:31:58 -0700

    implement redux-logger for debugging

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { routerMiddleware } from 'react-router-redux'
+import { createLogger } from 'redux-logger'
 import reducers from '../reducers'
 
+let middlewares = [thunk, routerMiddleware(history)];
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger({
+    collapsed: (getState, action, logEntry) => !logEntry.error
+  })
+
+  middlewares.push(logger)
+}
+
 export default (history) => {
-  let middlewares = [thunk, routerMiddleware(history)];
   let store = createStore(reducers, applyMiddleware(...middlewares));
 
   return store;
